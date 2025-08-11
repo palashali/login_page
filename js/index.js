547991c124086424eb904d8a781caf8a7a34b418
PALASH ALI <mymac@MacBook-Pro.local>
2025-08-11 22:49:13 +0900

    login page

---
+++ js/index.js
@@ -0,0 +1,12 @@
+document.getElementById("submit-button").addEventListener("click", function () {
+  const userEmailField = document.getElementById("user-email");
+  const email = userEmailField.value;
+
+  const userPasswordField = document.getElementById("user-password");
+  const password = userPasswordField.value;
+  if (email == "admin@gmail.com" && password == 123456) {
+    window.location.href = "./bank/halim.html";
+  }
+  console.log("Email: " + email);
+  console.log("Password: " + password);
+});
